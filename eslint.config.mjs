4e07aa09bdaf03d344505b932c8489055ddedf1b
Ruslan <axelezgames@gmail.com>
2024-07-17 23:38:13 +0500

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
     languageOptions: {
       parserOptions: {
